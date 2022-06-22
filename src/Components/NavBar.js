e706181a1ca332a6b20b950e1c3ad616695fa619
Laiba Sajid Khan <sajid.laiba378@gmail.com>
2022-06-21 23:37:00 -0400

    added some navBar styling

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,35 +1,42 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import "../Styles/NavBar.css";
-import HomeIcon from '@mui/icons-material/Home';
+import logoImage from "../assets/logo_transparent.png"
 import SearchIcon from '@mui/icons-material/Search';
+import { IconButton } from '@mui/material';
+import PersonIcon from '@mui/icons-material/Person';
+import {Button} from '@mui/material';
 
 function NavBar() {
 
   return (
-    <nav>
-        <div className='navbar-container'>
+    <nav className='navbar-container'>
 
-            <div className='navbar-home-info'>
-                <Link to="/"> <HomeIcon/></Link>
-                <div className="navbar-text">Reservation-App</div>
+            <div className='info-image-box'>
+                <Link to="/"> <img src={logoImage} alt="link to home page"/> </Link>
             </div>
-            
+        
+            <div className='form-container'>
+                <form className='search-form'>
+                    <IconButton className="search-button" aria-label="search by restaurant name or cuisine type" color="primary">
+                        <SearchIcon />
+                    </IconButton>
+                    
+                    <input
+                        className='search-input'
+                        placeholder='Cuisine, food, drinks, etc '
+                        type="text"
+                        onInput={""}
+                        value={""}
+                    />
+                </form>
+            </div>
 
-            <form className='navbar-input'>
-                <button className='navbar-inputButton'><SearchIcon/></button>
-                <input
-                    placeholder='...'
-                    type="text"
-                    onInput={""}
-                    value={""}
-                />
-            </form>
-            <div>
-                <button><Link to="/">Log in</Link></button>
+            <div className='login-box'>
+                <Button className="login-button" variant="outlined" startIcon={<PersonIcon/>} aria-label='sign into account'>
+                    <Link to="/">Log in</Link>
+                </Button>
             </div>
-            
-        </div>
     </nav>
   )
 }
