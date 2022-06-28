5c828dcb60db99ea89b01c6b8087179ef2b57350
Laiba Sajid Khan <sajid.laiba378@gmail.com>
2022-06-28 05:28:44 -0400

    added single restaurant styling

--- src/Components/Restaurant.js
+++ src/Components/Restaurant.js
@@ -3,6 +3,11 @@ import {Link} from "react-router-dom";
 import restaurantStockImg from "../assets/restaurant.png";
 import "../Styles/Restaurant.css";
 import { Typography } from '@mui/material';
+//import location, cuisine, money, clock
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import CreditCardOutlinedIcon from '@mui/icons-material/CreditCardOutlined';
+import AccessTimeOutlinedIcon from '@mui/icons-material/AccessTimeOutlined';
+import RestaurantOutlinedIcon from '@mui/icons-material/RestaurantOutlined';
 
 function Restaurant( {singleRestaurant} ) {
   const id = singleRestaurant.id;
@@ -15,24 +20,35 @@ function Restaurant( {singleRestaurant} ) {
           <img className="restaurant-image"  src={restaurantStockImg} alt='restaurant stock'/>
             
           <div className='restaurant-name'>
-              <Typography variant="h5" component="h2">{singleRestaurant.name}</Typography>
+              <Typography variant="h5" component="h2">
+                {singleRestaurant.name}
+              </Typography>
           </div>
              
               <div className='restaurant-type'>
-                  <div>
+                  <Typography>
+                    <RestaurantOutlinedIcon/>
                     {singleRestaurant.cuisine}
-                  </div>
+                  </Typography>
+
+                  <Typography>
+                    <AccessTimeOutlinedIcon/>
+                    {singleRestaurant.openingTime} - {singleRestaurant.closingTime}
+                  </Typography>
               </div>
               <div className='restaurant-location'>
-                  <div>
+                  <Typography>
+                  <LocationOnIcon/>
                     {singleRestaurant.location}
-                  </div>
+                  </Typography>
               </div>
             <div className='restaurant-price'>
-                  <div>
+                  <Typography>
+                      <CreditCardOutlinedIcon size="small"/>
                       {singleRestaurant.price}
-                  </div>
+                  </Typography>
             </div>
+
           </div>
       </Link> 
     </div>
