6b2fc6892dd0d929b70b1fd7ee23ce0965fec83b
Laiba Sajid Khan <sajid.laiba378@gmail.com>
2022-06-28 04:00:10 -0400

    made filterBar sticky

--- src/Components/FilterBar.js
+++ src/Components/FilterBar.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import FilterOptions from './FilterOptions';
-import "../Styles/FilterBar.css"
+import "../Styles/FilterBar.css";
+import { Typography } from '@mui/material';
 
 function FilterBar({filterCategoryNames, filterButtonValue, setFilterButtonValue}) {
   return (
     <div className='filterBar-container'>
-      <div className='filterBar-title'>All Stores</div>
+      <Typography variant="h5" component="h2" className='filterBar-title'>
+        All Stores
+      </Typography>
         <FilterOptions filterButtonValue={filterButtonValue} setFilterButtonValue={setFilterButtonValue} filterCategoryNames={filterCategoryNames}/>
     </div>
   );
