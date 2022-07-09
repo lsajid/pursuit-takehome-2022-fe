0b075d51a8aa0f0396c2ff627dff4da48cf9b709
Laiba Sajid Khan <sajid.laiba378@gmail.com>
2022-07-09 19:36:49 -0400

    fixed time bug

--- src/util/formatReservationTime.js
+++ src/util/formatReservationTime.js
@@ -5,20 +5,28 @@ import formatTime from "./formatTime";
  * 2022-05-11T18:00:00.000Z ----> 12:00AM
  * 
  * 2022-07-08T19:43:00.000Z ----> 7:43PM
- * 
+ * dateInUTC.toLocaleDateString('en-US', { timeZone: 'UTC' })
  * @param {string} dateIso
  * @returns {string} formated to Time
  */
 
  const formatReservationTime = (dateIso) => {
     let dateInUTC = new Date(dateIso);
-    let hours = dateInUTC.getHours();
-    let minutes = dateInUTC.getMinutes();
-    let timeStr = hours + ":" + minutes;
-    
-    return formatTime(timeStr)
+    let stringDate = dateInUTC.toLocaleTimeString('en-US', { timeZone: 'UTC' })
+    let stringArr = stringDate.split(":").map((el, index) => {
+        console.log(el)
+        if(el === '00 PM'){
+            return 'PM'
+        }else{
+            if(index === 0){
+                return el+ ":";
+            }
+            return el
+        }
+    }).join(" ");
+
+
+     return stringArr;
 }
-
-
 
 export default formatReservationTime;
